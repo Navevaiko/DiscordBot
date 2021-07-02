850b8968cecbee17a5bf5352149d90f9d603ebb4
Gabriel Navevaiko <gabriel.navevaiko@primi.local>
2021-07-02 14:00:45 -0300

    chore: First commit

---
+++ commands.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+@commands.command()
+async def play(ctx, user_id):
+  pass
+
+@commands.command()
+async def end(ctx):
+  pass
+
+@commands.command()
+async def place(ctx):
+  pass
---
+++ main.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import commands as bot_commands
+import os 
+
+load_dotenv()
+bot = commands.Bot(command_prefix='!')
+
+bot.add_command(bot_commands.play)
+bot.add_command(bot_commands.end)
+bot.add_command(bot_commands.place)
+
+bot.run(os.getenv('DISCORD_TOKEN'))
